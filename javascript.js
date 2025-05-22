d2b39ad8e0f7a5ec776eb357e3aeb22428ba0d54
marodacco <48792073+marodacco@users.noreply.github.com>
2025-05-22 14:01:35 +0900

    Change how to create boxes
    Changed the script to create boxes using flex-wrap
    with a fixed width
    Created a function to make boxes
    Added a button for user to set the number of boxes

--- javascript.js
+++ javascript.js
@@ -1,26 +1,53 @@
+// Set boxes width
+const boxesWidth = 640;
+
+// Default number of boxes
+const defaultNumber = 16;
+
 // Get the container
 const container = document.querySelector("#container");
 
-// Create 16 rows
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.setAttribute("class", "row");
+// Create an inner container for boxes
+const boxes = document.createElement("div");
+boxes.id = "boxes";
+boxes.style.width = boxesWidth + "px";
 
-  // Add 16 boxes in a row
-  for (let i = 0; i < 16; i++) {
+createBoxes(defaultNumber);
+
+// Get the button
+const button = document.querySelector("button");
+
+// Set the button to change the number of boxes
+button.addEventListener("click", () => {
+  const userNumber = prompt("the number of squares per side:");
+  console.log(userNumber);
+
+  // Remove the current boxes
+  const oldBoxes = document.querySelectorAll("#boxes div");
+  oldBoxes.forEach((oldBox) => {
+    boxes.removeChild(oldBox);
+  });
+
+  // Create new boxes
+  createBoxes(userNumber);
+});
+
+// Create boxes
+function createBoxes(num) {
+  for (let i = 0; i < num * num; i++) {
     const box = document.createElement("div");
     box.setAttribute("class", "box");
+    // Set box width and height
+    box.style.width = boxesWidth / num + "px";
+    box.style.height = boxesWidth / num + "px";
 
     // Add event listner
     box.addEventListener("mouseenter", () => {
-      box.style.backgroundColor = "green";
+    box.style.backgroundColor = "green";
     });
-
-    row.appendChild(box);
+    
+    boxes.appendChild(box);
   }
 
-  container.appendChild(row);
+  container.appendChild(boxes);
 }
-
-
-
